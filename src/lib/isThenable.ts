9839a5514beb3116b5a6c0a421a3cbbd722d5a2e
Antonio Román <kyradiscord@gmail.com>
2019-11-27 00:44:24 +0100

    fix: isThenable requires objects, added type guarding

--- src/lib/isThenable.ts
+++ src/lib/isThenable.ts
@@ -1,5 +1,10 @@
 import isFunction from './isFunction';
 
+export interface Thenable {
+	then: Function;
+	catch: Function;
+}
+
 function hasThen(input: { then?: Function }): boolean {
 	return Reflect.has(input, 'then') && isFunction(input.then);
 }
@@ -12,8 +17,8 @@ function hasCatch(input: { catch?: Function }): boolean {
  * Verify if an object is a promise.
  * @param input The promise to verify
  */
-export default function isThenable(input: unknown): boolean {
-	if (!input) return false;
+export default function isThenable(input: unknown): input is Thenable {
+	if (typeof input !== 'object' || input === null) return false;
 	return (input instanceof Promise) ||
 		(input !== Promise.prototype && hasThen(input) && hasCatch(input));
 }
