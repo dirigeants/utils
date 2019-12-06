335895ec1db9a6be274f70c05f272c2794790aec
Antonio Román <kyradiscord@gmail.com>
2019-12-06 08:50:46 +0100

    fix: Use reflection to check if a method actually exists (#34)
    * fix: Use reflection to check if a method actually exists
    
    * fix: isThenable requires objects, added type guarding
    
    * lint: Resolved ESLint error

--- src/lib/isThenable.ts
+++ src/lib/isThenable.ts
@@ -1,20 +1,24 @@
 import isFunction from './isFunction';
-import isObject from './isObject';
 
-function hasThen(input: unknown | { then?: Function }): boolean {
-	return isObject(input) && isFunction(input.then);
+export interface Thenable {
+	then: Function;
+	catch: Function;
 }
 
-function hasCatch(input: unknown | { catch?: Function }): boolean {
-	return isObject(input) && isFunction(input.catch);
+function hasThen(input: { then?: Function }): boolean {
+	return Reflect.has(input, 'then') && isFunction(input.then);
+}
+
+function hasCatch(input: { catch?: Function }): boolean {
+	return Reflect.has(input, 'catch') && isFunction(input.catch);
 }
 
 /**
  * Verify if an object is a promise.
  * @param input The promise to verify
  */
-export default function isThenable(input: unknown): boolean {
-	if (!input) return false;
+export default function isThenable(input: unknown): input is Thenable {
+	if (typeof input !== 'object' || input === null) return false;
 	return (input instanceof Promise) ||
 		(input !== Promise.prototype && hasThen(input) && hasCatch(input));
 }
