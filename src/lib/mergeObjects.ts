e479b27c4588dbfaf4be1a6bf2c8e57aaeb28946
Antonio Román <kyradiscord@gmail.com>
2020-05-25 17:55:17 +0200

    fix(MergeObjects): make the interface more correct and better (#184)

--- src/lib/mergeObjects.ts
+++ src/lib/mergeObjects.ts
@@ -1,20 +1,19 @@
 import { isObject } from './isObject';
 
-type KeyedObject = Record<PropertyKey, unknown>;
-
 /**
  * Merges two objects
  * @param objTarget The object to be merged
  * @param objSource The object to merge
  */
-export function mergeObjects<A extends KeyedObject, B extends KeyedObject>(objTarget: A, objSource: B): A & B {
-	for (const [key, value] of Object.entries(objSource) as [keyof B, unknown][]) {
-		const targetValue = objTarget[key];
+export function mergeObjects<A extends object, B extends object>(objTarget: A, objSource: Readonly<B>): A & B {
+	for (const [key, value] of Object.entries(objSource)) {
+		const targetValue = Reflect.get(objTarget, key);
 		if (isObject(value)) {
-			Reflect.set(objTarget, key, isObject(targetValue) ? mergeObjects(targetValue as KeyedObject, value as KeyedObject) : value);
+			Reflect.set(objTarget, key, isObject(targetValue) ? mergeObjects(targetValue, value as object) : value);
 		} else if (!isObject(targetValue)) {
 			Reflect.set(objTarget, key, value);
 		}
 	}
+
 	return objTarget as A & B;
 }
